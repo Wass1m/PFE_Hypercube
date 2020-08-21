507fd1689ca37c9c5f07b2546f21236f0574effe
MACBOOK <fm_ghernaout@esi.dz>
2020-08-21 14:30:10 +0100

    tests added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import TestPrincipal from "./components/test_principale";
 import TestPrincipal_Etude from "./components/test_principale_etude";
 import TestPrincipal_Etude2 from "./components/etude2";
+import TestPrincipal_fourmis from "./components/nbr_fourmis_etude";
 function App() {
   return (
     <Router>
       <Route exact path="/" component={MainInterface} />
       <Route exact path="/main-final" component={TestPrincipal} />
-      <Route exact path="/main" component={TestPrincipal_Etude} />
+      <Route exact path="/main" component={TestPrincipal_fourmis} />
       <Route exact path="/main2" component={TestPrincipal_Etude2} />
     </Router>
   );
